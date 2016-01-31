a33b57ba192e9d5fc78d782965bf14cfdaee54b4
Katie Fritz <katie@katiemfritz.com>
2016-01-31 18:52:54 -0500

    remove posts model because we're not using it

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   get '/sign-in' => 'sessions#new'
   delete '/sign-out' => 'sessions#destroy'
 
   root 'static#home'
 
   resources :books
-  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131165845) do
+ActiveRecord::Schema.define(version: 20160131234357) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",      default: "",    null: false
@@ -19,14 +19,6 @@ ActiveRecord::Schema.define(version: 20160131165845) do
     t.boolean  "ebook",      default: false, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.boolean  "is_published"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
