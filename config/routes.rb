c7f97639e37151158ba5d824ae9d00d6bdc3bba8
Katie Fritz <katie@katiemfritz.com>
2016-06-08 19:46:22 -0400

    working nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :sessions, only: [:new, :create, :destroy]
-
   get '/sign-in' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
   delete '/sign-out' => 'sessions#destroy'
 
   root 'static#home'
 
+  get '/books' => 'books#index'
+  
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
